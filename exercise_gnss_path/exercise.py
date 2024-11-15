62ca4a20d5e8b33de5f72e8404363dafddb20a95
VHelbig <43109146+VHelbig@users.noreply.github.com>
2024-11-15 12:32:04 +0100

    Initial Commit

---
+++ exercise_gnss_path/exercise.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+from libs.utm import utmconv
+from libs.exportkml import kmlclass
+from scipy.ndimage import convolve
+from PathManagement import PathManagement
+
+folder="exercise_gnss_path/"
+filePath=folder+"gps_log-copy.csv"
+PathManager=PathManagement()
+
+PathManager.LoadPath(filePath)
+PathManager.ConvertToUTM()
+max_speed=10 #m/s
+PathManager.RemoveOutliers(max_speed)
+PathManager.ComputeSimplifyMap()
+PathManager.SimplifyPath(10)
+PathManager.ConvertToGeodedic()
+PathManager.ExportKml("output.kml")
